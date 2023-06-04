8ccdc90657eb005548666cf90d0c1bb4dcc3ec4a
Dev. Sanches <dev.anaclarasanches@gmail.com>
2023-06-04 15:27:20 -0300

    function for each prompt

--- todo-list-cmd.py
+++ todo-list-cmd.py
@@ -1,9 +1,36 @@
-PROMPT = 'Type add, show, edit, complete or exit: '
+def add(
+
+):
+    pass
+
+
+def show():
+    pass
+
+
+def edit():
+    pass
+
+
+def complete():
+    pass
+
+
+PROMPT_DICT = {
+    'add': add,
+    'show': show,
+    'edit': edit,
+    'complete': complete,
+}
+
+PROMPT = 'Type add, show, edit, complete or exit:\n> '
 todo_list = []
 user_input = None
 
 while user_input != 'exit':
     user_input = input(PROMPT).lower()
-    new_todo = input('TO-DO: ')
-    todo_list.append(new_todo)
-    print(todo_list)
+    for prompt in PROMPT_DICT:
+        if user_input == prompt:
+            PROMPT_DICT[prompt]()
+    # new_todo = input('TO-DO: ')
+    # todo_list.append(new_todo)
