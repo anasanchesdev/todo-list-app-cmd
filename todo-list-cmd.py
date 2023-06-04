748378958c242a16bf9023a3d3032101d29460c9
Dev. Sanches <dev.anaclarasanches@gmail.com>
2023-06-04 14:42:22 -0300

    program pase

--- todo-list-cmd.py
+++ todo-list-cmd.py
@@ -1,5 +1,8 @@
-prompt = 'Type add, show, edit, complete or exit: '
-user_input = input(prompt)
-"""
-hello
-"""
+PROMPT = 'Type add, show, edit, complete or exit: '
+todo_list = []
+
+while PROMPT != 'exit':
+    user_input = input(PROMPT).lower()
+    new_todo = input('TO-DO: ')
+    todo_list.append(new_todo)
+    print(todo_list)
