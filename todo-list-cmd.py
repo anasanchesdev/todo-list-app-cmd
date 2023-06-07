d49c46f8530cf874665fc05e48d53500ee43ce30
Dev. Sanches <dev.anaclarasanches@gmail.com>
2023-06-07 10:45:16 -0300

    [🧠] Structure change: functions to match/case

--- todo-list-cmd.py
+++ todo-list-cmd.py
@@ -1,34 +1,33 @@
-def add():
-    pass
+def show(todos):
+    """
+    Enumerates and prints items of todo list 
+    """
+    for index, todo in enumerate(todo_list):
+                print(f'{index+1}.', todo)
 
 
-def show():
-    pass
-
-
-def edit():
-    pass
-
-
-def complete():
-    pass
-
-
-PROMPT_DICT = {
-    'add': add,
-    'show': show,
-    'edit': edit,
-    'complete': complete,
-}
-
-PROMPT = 'Type add, show, edit, complete or exit:\n> '
 todo_list = []
 user_input = None
 
 while user_input != 'exit':
-    user_input = input(PROMPT).lower()
-    for prompt in PROMPT_DICT:
-        if user_input == prompt:
-            PROMPT_DICT[prompt]()
-    # new_todo = input('TO-DO: ')
-    # todo_list.append(new_todo)
+    user_input = input('\nType add, show, edit, complete or exit:\n > ').lower()
+    match user_input:
+        case 'add':
+            todo_added = input('Enter a todo:\n > ').capitalize()
+            todo_list.append(todo_added)
+        case 'show':
+            show(todo_list)
+        case 'edit':
+            show(todo_list)
+            choosen_todo_index = input('Choose an item to edit (type its number):\n > ')
+            if choosen_todo_index.isnumeric():  # prevents bugs
+                 choosen_todo_index = int(choosen_todo_index) - 1
+
+            todo_edited = input('Enter the new todo:\n > ').capitalize()
+            for todo in todo_list: 
+                old_todo_index = todo_list.index(todo)    
+                if old_todo_index == choosen_todo_index:
+                    todo_list[old_todo_index] = todo_edited
+        case 'complete':
+            todo_completed = input('Enter the completed to-do (type its number):')
+            
